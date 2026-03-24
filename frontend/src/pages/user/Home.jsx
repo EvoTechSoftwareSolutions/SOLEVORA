a98d1e86514f2c3e06d44fc0c54f69f9da150fcd
Sajeevan-Varatharasa <sajeevan@evotech.lk>
2026-03-24 16:44:54 +0530

    designs ad and faqs

--- frontend/src/pages/user/Home.jsx
+++ frontend/src/pages/user/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "../../styles/user/Home.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
@@ -10,8 +11,10 @@ import {
   ShoppingCartIcon,
   FaSearch,
   HiOutlineAdjustmentsHorizontal,
+  FaPlus 
 } from "../../components/common/icons";
 import shoe from "../../assets/image/orangeshoe.png";
+import greenshoe from "../../assets/image/greenshoe.png";
 import premium from "../../assets/image/premium.svg";
 import happyface from "../../assets/image/happyface.svg";
 import footstep from "../../assets/image/footstep.svg";
@@ -34,23 +37,76 @@ import underarmour from "../../assets/image/LogoSvg/underarmour.svg";
 import nike from "../../assets/image/LogoSvg/nike.svg";
 import justdoit from "../../assets/image/LogoSvg/justdoit.svg";
 
-
-
-
-
 const categories = [
-  { name: "Office", color: "#ED7777", image: office, className:"category-circle__img" },
-  { name: "Boots", color: "#C9A3A3", image: boots, className:"category-circle__img" },
-  { name: "Brands", color: "#71ABE2", image: brands, className:"category-circle__img-rotate" },
-  { name: "Convers", color: "#DF0B0B", image: convers, className:"category-circle__img" },
-  { name: "Sandals", color: "#FAE25A", image: sandals, className:"category-circle__img-rotate"},
-  { name: "Casual", color: "#ED7777", image: shoe, className:"category-circle__img-rotate"},
-  { name: "Kids", color: "#fff", image: kids, className:"category-circle__img-rotate"},
-  { name: "Sports", color: "#FD9C50", image: sports, className:"category-circle__img-rotate"},
-  { name: "Ladies", color: "#D771D0", image: girls, className:"category-circle__img"},
-  { name: "Sneakers", color: "#75DF8C", image: sneakers ,className:"category-circle__img-rotate"},
+  {
+    name: "Office",
+    color: "#ED7777",
+    image: office,
+    className: "category-circle__img",
+  },
+  {
+    name: "Boots",
+    color: "#C9A3A3",
+    image: boots,
+    className: "category-circle__img",
+  },
+  {
+    name: "Brands",
+    color: "#71ABE2",
+    image: brands,
+    className: "category-circle__img-rotate",
+  },
+  {
+    name: "Convers",
+    color: "#DF0B0B",
+    image: convers,
+    className: "category-circle__img",
+  },
+  {
+    name: "Sandals",
+    color: "#FAE25A",
+    image: sandals,
+    className: "category-circle__img-rotate",
+  },
+  {
+    name: "Casual",
+    color: "#ED7777",
+    image: shoe,
+    className: "category-circle__img-rotate",
+  },
+  {
+    name: "Kids",
+    color: "#fff",
+    image: kids,
+    className: "category-circle__img-rotate",
+  },
+  {
+    name: "Sports",
+    color: "#FD9C50",
+    image: sports,
+    className: "category-circle__img-rotate",
+  },
+  {
+    name: "Ladies",
+    color: "#D771D0",
+    image: girls,
+    className: "category-circle__img",
+  },
+  {
+    name: "Sneakers",
+    color: "#75DF8C",
+    image: sneakers,
+    className: "category-circle__img-rotate",
+  },
 ];
+
+
 const Home = () => {
+  const [openIndex, setOpenIndex] = useState(null);
+
+const toggleFAQ = (index) => {
+  setOpenIndex(openIndex === index ? null : index);
+};
   return (
     <div className="main-container">
       <div className="banner">
@@ -134,112 +190,262 @@ const Home = () => {
             </Link>
           </div>
         </div>
-      <div className="circle-grid">
-  {categories.map((cat, index) => (
-    <div
-      key={index}
-      className="category-circle"
-      style={{ backgroundColor: cat.color }}
-    >
-      <img
-        src={cat.image}
-        alt={cat.name}
-        className={cat.className}
-      />
-      <span className="category-circle__label">{cat.name}</span>
-    </div>
-  ))}
-</div>
+        <div className="circle-grid">
+          {categories.map((cat, index) => (
+            <div
+              key={index}
+              className="category-circle"
+              style={{ backgroundColor: cat.color }}
+            >
+              <img src={cat.image} alt={cat.name} className={cat.className} />
+              <span className="category-circle__label">{cat.name}</span>
+            </div>
+          ))}
+        </div>
       </section>
 
       <section className="brands">
         <h1>Top Brands</h1>
- <Swiper
-  modules={[Autoplay]}
-   speed={4000}
-  slidesPerView={6}
-  loop={true}
-  autoplay={{ 
-    delay: 0,
-    disableOnInteraction: false
-  }}
-  breakpoints={{
-    260: { slidesPerView: 2 },
-    320: { slidesPerView: 2 },
-    640: { slidesPerView: 3 },
-    1024: { slidesPerView: 6 },
-  }}
->
-  <SwiperSlide><img src={zara} alt="Zara" /></SwiperSlide>
-  <SwiperSlide><img src={adidas} alt="Adidas" /></SwiperSlide>
-  <SwiperSlide><img src={gucci} alt="Gucci" /></SwiperSlide>
-  <SwiperSlide><img src={lacoste} alt="Lacoste" /></SwiperSlide>
-  <SwiperSlide><img src={underarmour} alt="Under Armour" /></SwiperSlide>
-  <SwiperSlide><img src={nike} alt="Nike" /></SwiperSlide>
-  <SwiperSlide><img src={justdoit} alt="Just Do It" /></SwiperSlide>
-  <SwiperSlide><img src={jordan} alt="Jordan" /></SwiperSlide>
-  <SwiperSlide><img src={zara} alt="Zara" /></SwiperSlide>
-  <SwiperSlide><img src={adidas} alt="Adidas" /></SwiperSlide>
-  <SwiperSlide><img src={gucci} alt="Gucci" /></SwiperSlide>
-  <SwiperSlide><img src={lacoste} alt="Lacoste" /></SwiperSlide>
-  <SwiperSlide><img src={underarmour} alt="Under Armour" /></SwiperSlide>
-  <SwiperSlide><img src={nike} alt="Nike" /></SwiperSlide>
-  <SwiperSlide><img src={justdoit} alt="Just Do It" /></SwiperSlide>
-  <SwiperSlide><img src={jordan} alt="Jordan" /></SwiperSlide>
-</Swiper>
+        <Swiper
+          modules={[Autoplay]}
+          speed={4000}
+          slidesPerView={6}
+          loop={true}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            260: { slidesPerView: 2 },
+            320: { slidesPerView: 2 },
+            640: { slidesPerView: 3 },
+            1024: { slidesPerView: 6 },
+          }}
+        >
+          <SwiperSlide>
+            <img src={zara} alt="Zara" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={adidas} alt="Adidas" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={gucci} alt="Gucci" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={lacoste} alt="Lacoste" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={underarmour} alt="Under Armour" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={nike} alt="Nike" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={justdoit} alt="Just Do It" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={jordan} alt="Jordan" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={zara} alt="Zara" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={adidas} alt="Adidas" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={gucci} alt="Gucci" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={lacoste} alt="Lacoste" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={underarmour} alt="Under Armour" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={nike} alt="Nike" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={justdoit} alt="Just Do It" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={jordan} alt="Jordan" />
+          </SwiperSlide>
+        </Swiper>
       </section>
 
       <section className="why-us">
-  <div className="why-us-intro">
-    <h1>Why Shop With Us</h1>
+        <div className="why-us-intro">
+          <h1>Why Shop With Us</h1>
+          <p>
+            Our collection focuses on premium materials, modern design, and
+            everyday comfort. We make it easy for you to find the perfect pair
+            of shoes that fits your lifestyle.
+          </p>
+          <Link to="/aboutUs" className="about__button">
+            Learn More
+          </Link>
+        </div>
+
+        <div className="card-box">
+          <div className="why-us-card">
+            <div className="why-us-card-icon">
+              <img src={trust} alt="Icon" />
+            </div>
+            <h3>Trusted Comfort</h3>
+            <p>
+              Our shoes are designed to deliver lasting comfort and dependable
+              quality, so every step you take feels supported and confident
+              throughout the day.
+            </p>
+          </div>
+          <div className="why-us-card long">
+            <div className="why-us-card-icon">
+              <img src={happyface} alt="Icon" />
+            </div>
+            <h3>Reliable Comfort</h3>
+            <p>
+              Built for everyday wear, our footwear combines comfort,
+              durability, and style to support you wherever your journey takes
+              you.
+            </p>
+          </div>
+          <div className="why-us-card long">
+            <div className="why-us-card-icon">
+              <img src={premium} alt="Icon" />
+            </div>
+            <h3>Premium Footwear</h3>
+            <p>
+              We bring you carefully selected footwear made with premium
+              materials and modern design to ensure both style and durability in
+              every pair.
+            </p>
+          </div>
+          <div className="why-us-card">
+            <div className="why-us-card-icon">
+              <img src={footstep} alt="Icon" />
+            </div>
+            <h3>Quality Steps</h3>
+            <p>
+              Every pair is crafted with attention to detail, giving you
+              reliable performance, long-lasting quality, and comfort in every
+              step.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="banner-2">
+        <h1>Own the Street</h1>
+        <p>Explore Men’s Footwear Trends</p>
+        <Link to="/category" className="shop-button-2">
+          Shop Now
+        </Link>
+      </section>
+
+      <section className="faq-containers">
+  <h2>Frequently Asked Questions</h2>
+
+  <div className="search-faq">
+    <FaSearch className="faq-icon" />
+    <input type="text" placeholder="Search..." />
+  </div>
+
+<div className="faqs">
+  <div className="questions" onClick={() => toggleFAQ(0)}>
+    <h4>Do you offer free shipping?</h4>
+    <FaPlus className={`dropdown ${openIndex === 0 ? "active" : ""}`} />
+  </div>
+
+  <p className={`answer ${openIndex === 0 ? "show" : ""}`}>
+    Yes, we provide free shipping on selected orders. Delivery times may vary depending on your location.
+  </p>
+</div>
+
+<div className="faqs">
+  <div className="questions" onClick={() => toggleFAQ(1)}>
+    <h4>Do you provide outside free shipping?</h4>
+    <FaPlus className={`dropdown ${openIndex === 1 ? "active" : ""}`} />
+  </div>
+
+  <p className={`answer ${openIndex === 1 ? "show" : ""}`}>
+    Yes, we provide free shipping on selected orders. Delivery times may vary depending on your location.
+  </p>
+</div>
+<div className="faqs">
+  <div className="questions" onClick={() => toggleFAQ(1)}>
+    <h4>How long does delivery take?</h4>
+    <FaPlus className={`dropdown ${openIndex === 1 ? "active" : ""}`} />
+  </div>
+  <p className={`answer ${openIndex === 1 ? "show" : ""}`}>
+    Delivery usually takes 3–7 business days depending on your location.
+  </p>
+</div>
+
+<div className="faqs">
+  <div className="questions" onClick={() => toggleFAQ(2)}>
+    <h4>Can I return or exchange products?</h4>
+    <FaPlus className={`dropdown ${openIndex === 2 ? "active" : ""}`} />
+  </div>
+  <p className={`answer ${openIndex === 2 ? "show" : ""}`}>
+    Yes, we offer easy returns and exchanges within 7 days of delivery.
+  </p>
+</div>
+
+<div className="faqs">
+  <div className="questions" onClick={() => toggleFAQ(3)}>
+    <h4>Are your products original?</h4>
+    <FaPlus className={`dropdown ${openIndex === 3 ? "active" : ""}`} />
+  </div>
+  <p className={`answer ${openIndex === 3 ? "show" : ""}`}>
+    All our products are 100% authentic and sourced from trusted suppliers.
+  </p>
+</div>
+
+<div className="faqs">
+  <div className="questions" onClick={() => toggleFAQ(4)}>
+    <h4>Do you offer discounts or promotions?</h4>
+    <FaPlus className={`dropdown ${openIndex === 4 ? "active" : ""}`} />
+  </div>
+  <p className={`answer ${openIndex === 4 ? "show" : ""}`}>
+    Yes, we regularly offer discounts and seasonal promotions.
+  </p>
+</div>
+
+<div className="faqs">
+  <div className="questions" onClick={() => toggleFAQ(5)}>
+    <h4>How can I contact customer support?</h4>
+    <FaPlus className={`dropdown ${openIndex === 5 ? "active" : ""}`} />
+  </div>
+  <p className={`answer ${openIndex === 5 ? "show" : ""}`}>
+    You can contact us via email or phone, and our support team will assist you promptly.
+  </p>
+</div>
+</section>
+
+<section className="ads">
+      <div className="yellow-banner">
+  <div className="banner-details">
+    <span className="tag">Today only</span>
+    <h3>Buy One Get One Free</h3>
     <p>
-      Our collection focuses on premium materials, modern design, and everyday comfort. 
-      We make it easy for you to find the perfect pair of shoes that fits your lifestyle.
+      Mix and match from our entire collection. No minimum spend required.
     </p>
-    <Link to="/aboutUs" className="about__button">
-      Learn More
+
+    <Link to="/category" className="deal-btn">
+      Grab The Deal
     </Link>
   </div>
 
-  <div className="card-box">
-      <div className="why-us-card">
-        <div className="why-us-card-icon">
-          <img src={trust} alt="Icon" />
-        </div>
-        <h3>Trusted Comfort</h3>
-        <p>
-          Our shoes are designed to deliver lasting comfort and dependable quality, 
-          so every step you take feels supported and confident throughout the day.
-        </p>
-      </div>
-     <div className="why-us-card long">
-        <div className="why-us-card-icon">
-          <img src={happyface} alt="Icon" />
-        </div>
-        <h3>Reliable Comfort</h3>
-        <p>
-          Built for everyday wear, our footwear combines comfort, durability, and style to support you wherever your journey takes you.
-        </p>
-      </div>
-       <div className="why-us-card long">
-        <div className="why-us-card-icon">
-          <img src={premium} alt="Icon" />
-        </div>
-        <h3>Premium Footwear</h3>
-        <p>
-       We bring you carefully selected footwear made with premium materials and modern design to ensure both style and durability in every pair.
-        </p>
-      </div>
-       <div className="why-us-card">
-        <div className="why-us-card-icon">
-          <img src={footstep} alt="Icon" />
-        </div>
-        <h3>Quality Steps</h3>
-        <p>
-         Every pair is crafted with attention to detail, giving you reliable performance, long-lasting quality, and comfort in every step.
-        </p>
-      </div>
+  <div className="banner-img">
+    <img src={greenshoe} alt="offer" />
   </div>
+
+  <div className="offer-circle">
+    <span>BUY 1</span>
+    <h3>GET</h3>
+    <span>FREE</span>
+  </div>
+</div>
 </section>
     </div>
   );
