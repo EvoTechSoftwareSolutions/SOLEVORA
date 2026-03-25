bc66b009b3a508455b4c050b48f0389ab83a6ce2
Sajeevan-Varatharasa <sajeevan@evotech.lk>
2026-03-25 23:21:50 +0530

    create about page

--- frontend/src/routes/UserRoutes.jsx
+++ frontend/src/routes/UserRoutes.jsx
@@ -2,11 +2,13 @@ import React, { lazy } from 'react';
 import { Route, Navigate } from 'react-router-dom';
 import ProtectedRoute from '../components/ProtectedRoute';
 
+
 // Layout
 const UserLayout = lazy(() => import('../layouts/UserLayout/UserLayout'));
 
 // Pages
 const Home = lazy(() => import('../pages/user/Home'));
+const About = lazy(() => import('../pages/user/AboutUs'));
 const ProductDetail = lazy(() => import('../pages/user/ProductDetail'));
 const Category = lazy(() => import('../pages/user/Category'));
 const Cart = lazy(() => import('../pages/user/Cart'));
@@ -24,6 +26,7 @@ const UserRoutes = (
   <Route path="/" element={<UserLayout />}>
     <Route index element={<Navigate to="/home" replace />} />
     <Route path="home" element={<Home />} />
+    <Route path="about" element={<About />} />
     <Route path="product/:id" element={<ProductDetail />} />
     <Route path="category" element={<Category />} />
     <Route path="cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
