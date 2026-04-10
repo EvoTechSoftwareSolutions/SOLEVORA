13f804de226d053612f5fdcec9b73e53be1c082b
Sajeevan-Varatharasa <sajeevan@evotech.lk>
2026-04-10 14:59:50 +0530

    solve auto scroll top

--- frontend/src/layouts/UserLayout/UserLayout.jsx
+++ frontend/src/layouts/UserLayout/UserLayout.jsx
@@ -1,11 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { useLocation, Outlet } from "react-router-dom";
 import Navbar from "../../components/common/Navbar";
 import logo from "../../assets/logo.png"
 import Footer from "../../components/common/Footer";
 import "../../styles/user/UserLayout.css";
 import "../../styles/user/Home.css";
+import { useEffect } from "react";
+
+
+
 
 const UserLayout = () => {
+    const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="layout-wrapper">
       <Navbar />
